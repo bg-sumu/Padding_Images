ef95952f5605e331edeac850a50738a421a4cc40
Sumukha B G <86544918+bg-sumu@users.noreply.github.com>
2022-08-06 17:04:18 +0530

    Update padding.py
    Made Minor Changes to the Code

--- padding.py
+++ padding.py
@@ -1,7 +1,9 @@
 #Code to add Padding to one Image
 import cv2 as cv
 
-path ="image.png" # Path of the Image
+path ="image.jpg" # Path of the Image
+
+# The Result can be seen in the Readme File Provided with this Repository
 
 img = cv.imread(path) #Image is Read here 
 
@@ -9,15 +11,17 @@ ht, wd, cc = img.shape #Calculating the Dimensions of the Image Array
 
 Padding_Color = [255,255,255] #Color of the Padding in RGB Format
 
-top  = 10
-bottom = 10
-left = 10
-right = 10
+#Type in the Amount of Padding you need on all Four Sides in pixels
+top  = 150
+bottom = 150
+left = 150
+right = 150
 
 
 new_img = cv.copyMakeBorder(img.copy(),top,bottom,left,right,cv.BORDER_CONSTANT,value=Padding_Color)
-
+type(new_img)
 
 # save result
-new_path = "Padded/" #Path to Store Padded Images
-cv.imwrite(new_path, new_img)
+padded_image = "image_padded.jpg" #Name of the Padded Image
+cv.imwrite(padded_image,new_img )
+
